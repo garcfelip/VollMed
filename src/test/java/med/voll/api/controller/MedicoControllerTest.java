c1bca20ad1859f5a508013c5196879e7776f7389
Felipe Garcia <felipegarc.tech@gmail.com>
2023-12-10 17:38:50 -0300

    feat(vollmed-api): first version of the vollmed api

---
+++ src/test/java/med/voll/api/controller/MedicoControllerTest.java
@@ -0,0 +1,90 @@
+package med.voll.api.controller;
+
+import med.voll.api.domain.medico.DadosCadastroMedico;
+import med.voll.api.domain.medico.DadosDetalhadoMedico;
+import med.voll.api.domain.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+class MedicoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<DadosCadastroMedico> dadosCadastroMedicoJson;
+
+    @Autowired
+    private JacksonTester<DadosDetalhadoMedico> dadosDetalhamentoMedicoJson;
+
+    @MockBean
+    private MedicoRepository repository;
+
+//    @Test
+//    @DisplayName("Deveria devolver codigo http 400 quando informacoes estao invalidas")
+//    @WithMockUser
+//    void cadastrar_cenario1() throws Exception {
+//        var response = mvc
+//                .perform(post("/medicos"))
+//                .andReturn().getResponse();
+//
+//        assertThat(response.getStatus())
+//                .isEqualTo(HttpStatus.BAD_REQUEST.value());
+//    }
+//
+//    @Test
+//    @DisplayName("Deveria devolver codigo http 200 quando informacoes estao validas")
+//    @WithMockUser
+//    void cadastrar_cenario2() throws Exception {
+//        var dadosCadastro = new DadosCadastroMedico(
+//                "Medico",
+//                "medico@voll.med",
+//                "61999999999",
+//                "123456",
+//                Especialidade.CARDIOLOGIA,
+//                dadosEndereco());
+//
+//        when(repository.save(any())).thenReturn(new Medico(dadosCadastro));
+//
+//        var response = mvc
+//                .perform(post("/medicos")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(dadosCadastroMedicoJson.write(dadosCadastro).getJson()))
+//                .andReturn().getResponse();
+//
+//        var dadosDetalhamento = new DadosDetalhadoMedico(
+//                null,
+//                dadosCadastro.nome(),
+//                dadosCadastro.email(),
+//                dadosCadastro.crm(),
+//                dadosCadastro.telefone(),
+//                dadosCadastro.especialidade(),
+//                new Endereco(dadosCadastro.endereco())
+//        );
+//        var jsonEsperado = dadosDetalhamentoMedicoJson.write(dadosDetalhamento).getJson();
+//
+//        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+//        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
+//    }
+//
+//    private DadosEndereco dadosEndereco() {
+//        return new DadosEndereco(
+//                "rua xpto",
+//                "bairro",
+//                "00000000",
+//                "Brasilia",
+//                "DF",
+//                null,
+//                null
+//        );
+//    }
+
+}
